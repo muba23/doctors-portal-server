4fbfda8b162e89e744fc4b85bada2d82ebe05d9a
muba23 <muba2307@gmail.com>
2021-11-06 16:57:23 +0600

    connected with client side

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9yewv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -14,8 +15,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
-        
-        
+        const database = client.db('doctors_poratl');
+        const appointmentsCollection = database.collection('appointments');
+
+        app.post('/appointments', async(req, res)=>{
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result);
+        });
     }
     finally{
        // await client.close();
@@ -28,5 +36,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`istening at ${port}`)
+  console.log(`listening at ${port}`)
 })
